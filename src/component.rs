f6e8ee187aa015cdcbfa63fd419cb597161cd864
Robert O'Shea <robertoshea2k11@gmail.com>
2024-01-18 17:28:16 +0000

    Added ToAny trait and components! macro

--- src/component.rs
+++ src/component.rs
@@ -1,6 +1,29 @@
-use crate::AToAny;
+pub trait ToAny {
+    fn to_any(&self) -> &dyn std::any::Any;
+}
 
-pub trait Component: AToAny {
+impl<T: 'static> ToAny for T {
+    fn to_any(&self) -> &dyn std::any::Any {
+        self
+    }
+}
+
+pub trait Component: ToAny {
     fn name(&self) -> String;
     fn update(&mut self);
 }
+
+#[macro_export]
+macro_rules! components {
+    [] => {
+        vec![]
+    };
+    [$head:expr] => {
+        vec![Box::new($head)]
+    };
+    [$($list:expr),*] => {
+        vec![$(Box::new($list)),*]
+    };
+}
+
+pub use components;
