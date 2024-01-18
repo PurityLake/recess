18a703910b0a3d5c8869ecb70783f9e8144ccb14
Robert O'Shea <robertoshea2k11@gmail.com>
2024-01-18 17:27:16 +0000

    Added testss to query.rs

--- src/query.rs
+++ src/query.rs
@@ -1,6 +1,7 @@
 use crate::component::Component;
 use crate::entity::Entity;
 
+#[derive(Default)]
 pub struct Query<Types: Default + Component + Clone + 'static> {
     pub types: Types,
 }
@@ -23,5 +24,45 @@ impl<Types: Default + Component + Clone + 'static> Query<Types> {
             }
         }
         Err(false)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::components;
+
+    #[allow(dead_code)]
+    #[derive(Default, Clone, Debug)]
+    struct Position {
+        pub x: i32,
+        pub y: i32,
+    }
+
+    impl Position {
+        fn new(x: i32, y: i32) -> Self {
+            Position { x, y }
+        }
+    }
+
+    impl Component for Position {
+        fn name(&self) -> String {
+            "Position".into()
+        }
+
+        fn update(&mut self) {
+            println!("HERE")
+        }
+    }
+
+    #[test]
+    fn test_query() {
+        let query = Query::<Position>::new();
+        let pos = Position::new(10, 10);
+        let e = Entity::new(0, "player".into(), components![pos]);
+        if let Ok(position) = query.query(e) {
+            assert!(position.x == 10);
+            assert!(position.y == 10);
+        }
     }
 }
